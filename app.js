d23eade3f2663660580df139ee2288fd445ec2f5
ahlaan <mohamedahlan58@gmail.com>
2025-03-08 19:43:53 +0530

    database setup

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ require("express-async-errors");
 
 const express = require("express");
 const errorHandler = require("./handlers/errorHandler");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
 const app = express();
+
+mongoose
+  .connect(process.env.mongo_connection, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(() => {
+    console.log("Error while connecting to MongoDB");
+  });
+
+//Models
+require("./models/users.models");
 
 app.use(express.json());
 
-//End od all routes...
+//End of all routes...
 app.use(errorHandler);
 
 app.listen(8000, () => {
---
+++ models/users.models.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const usersSchema = new mongoose.Schema({
+  full_name: {
+    type: String,
+    required: [true, "Name is Required"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password is Required"],
+  },
+
+  balance: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const UsersModel = mongoose.model("Users", usersSchema);
+
+module.exports = UsersModel;
